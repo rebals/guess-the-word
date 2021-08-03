11a4205e5c6e998785ee0724dea65b3873c672a8
rebals <rls.blue09@gmail.com>
2021-08-03 16:54:36 -0500

    Correction to placeholder

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
 
 const placeholder = function(word){
-    placeholderLetters = [];
+    const placeholderLetters = [];
       for (const letter of word){
         console.log(letter);
         placeholderLetters.push("●");
@@ -18,9 +18,16 @@ const placeholder = function(word){
     wordProgress.innerText = placeholderLetters.join("");
 };
 
+placeholder(word);
+
 guessButton.addEventListener("click", function(e){
     e.preventDefault();
     const guess = letterInput.value;
     console.log(guess);
     letterInput.value = "";
 });
+
+const checkPlayer = function(){
+    const acceptedLetter = /[a-zA-Z]/
+    
+}
